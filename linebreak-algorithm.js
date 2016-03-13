0b4c97f1985f954314135bbf2362ea172edaf0fa
Travis Millward <travismillward@mac.com>
2016-03-13 17:19:55 -0600

    even better.

--- linebreak-algorithm.js
+++ linebreak-algorithm.js
@@ -1,37 +1,35 @@
-"use strict";
+function applyLineBreaks(textarea) {
+	"use strict";
+	// Requires the css rule "line-break" to be "strict" or "loose". 
+	// {line-break: strict;}
+	// Also requires the attribte "wrap" to be "hard".
 
-var t = document.getElementsByTagName('textarea')[0];
-
-
-function getFontInfo(){
-	return {
-		family: window.getComputedStyle( t, null).getPropertyValue('font-family'),
-		pixels: window.getComputedStyle( t, null).getPropertyValue('font-size')
+	function getTextWidthCanvas(text, font) {
+		var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
+		document.body.appendChild(canvas);
+		var context = canvas.getContext("2d");
+		context.font = font;
+		var metrics = context.measureText(text);
+		return metrics.width;
 	}
-}
 
+	function getFontInfo(){
+		return {
+			family: window.getComputedStyle( textarea, null).getPropertyValue('font-family'),
+			pixels: window.getComputedStyle( textarea, null).getPropertyValue('font-size')
+		}
+	}
 
-function getTextWidthCanvas(text, font) {
-	var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
-	document.body.appendChild(canvas);
-	var context = canvas.getContext("2d");
-	context.font = font;
-	var metrics = context.measureText(text);
-	return metrics.width;
-}
+	var font = getFontInfo();
 
-var font = getFontInfo();
+	function getTextWidth(text) {
+		var width = getTextWidthCanvas(text, font.pixels + " " + font.family);
+		return width;
+	}
 
-function getTextWidth(text) {
-	var width = getTextWidthCanvas(text, font.pixels + " " + font.family);
-	return width;
-}
-
-
-function getTextareaWidth(){
 	var re=/\r\n|\n\r|\n|\r/g;
-	var lines = t.value.replace(re,"\n").split("\n");  // 4 lines
-	var maxLineWidth = t.offsetWidth;
+	var lines = textarea.value.replace(re,"\n").split("\n");  // 4 lines
+	var maxLineWidth = textarea.offsetWidth;
 	var spaceCharWidth = getTextWidth(" ");
 	var outer = [];
 	outer.push([]);
